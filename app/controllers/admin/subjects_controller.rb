9db15c4b52885401dd547c66784eaf3fb4ce271f
tuanvuP <tuanvu.workmail@gmail.com>
2019-07-02 14:59:35 +0700

    create user

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -1,4 +1,6 @@
 class Admin::SubjectsController < Admin::BaseController
+  before_action :load_subject, except: [:index, :new, :create]
+
   def new
     @subject = Subject.new
   end
@@ -18,9 +20,35 @@ class Admin::SubjectsController < Admin::BaseController
       per_page: Settings.subject_per_page
   end
 
+  def edit; end
+
+  def update
+    if @subject.update_attributes subject_params
+      flash[:success] = t "update_successfully"
+      redirect_to admin_subjects_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @subject.destroy
+      flash[:success] = t "delete_successfully"
+    else
+      flash.now[:danger] = t "error_message"
+    end
+    redirect_to admin_subjects_path
+  end
+
   private
 
   def subject_params
     params.require(:subject).permit :name, :description
+  end
+
+  def load_subject
+    @subject = Subject.find_by id: params[:id]
+    return if @subject
+    render file: "public/404.html", status: :not_found, layout: false
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,32 @@
 class Admin::UsersController < Admin::BaseController
   def destroy; end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    password = SecureRandom.urlsafe_base64(4)
+
+    @user = User.new user_params
+    @user.password = password
+    @user.password_confirmation = password
+    @user.role = 1 if params[:role] == "supervisor"
+
+    if @user.save
+      flash[:success] = t "create_successfully"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def index; end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
